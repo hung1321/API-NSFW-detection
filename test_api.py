f7a64cadcb2efc3a915f7244028f8586e8c6d216
hung1321 <98698290+hung1321@users.noreply.github.com>
2024-04-25 07:14:03 +0700

    Add files via upload

---
+++ test_api.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from youtube_transcript_api import YouTubeTranscriptApi
+from transformers import pipeline
+from huggingface_hub import login
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+tokenizer = AutoTokenizer.from_pretrained("michellejieli/NSFW_text_classifier")
+model = AutoModelForSequenceClassification.from_pretrained("michellejieli/NSFW_text_classifier")
+app = Flask(__name__)
+@app.route("/hello")
+def home():
+    return "Home"
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id":user_id,
+        "name":"John Doe",
+        "email":"john@gmail.com",
+    }
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+        return jsonify(user_data),200
+@app.route("/create-user",methods = ["POST"])
+def create_user():
+    login(token="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")
+    data = request.get_json()
+    url = data["url"]
+    video_id = url.split("=")[1]
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript = ' '.join([d['text'] for d in transcript_list])
+    transcript = transcript[:1024]
+    classifier = pipeline("text-classification", model="michellejieli/NSFW_text_classifier")
+    result = classifier(transcript)
+    res = {
+        "text":transcript,
+        "result":result
+    }
+    return jsonify(res),201
+if __name__ == "__main__":
+    app.run(debug= True)
